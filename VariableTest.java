94a28da88671da46e985efa223fba612a49efa10
HUAWEİ <s230446033@stu.thk.edu.tr>
2025-10-09 09:59:43 +0300

    init

---
+++ VariableTest.java
@@ -0,0 +1,27 @@
+public class VariableTest {
+   //First Question
+    static int counter=0;
+
+    public void incremnetsCounter(){
+        counter++;
+        System.out.println("Counter: "+counter);
+    }
+    public static void main(String[]args){
+         VariableTest obj1= new VariableTest();
+        obj1.incremnetsCounter();
+         VariableTest obj2=new VariableTest();
+        obj2.incremnetsCounter();
+        System.out.println();
+        System.out.println();
+ //Second Question
+
+
+
+
+
+    }
+
+
+
+    }
+
