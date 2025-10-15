c2c41bd6e58ce51bb57433f7d5861c8ce1cf5c9b
HUAWEİ <s230446033@stu.thk.edu.tr>
2025-10-15 21:42:24 +0300

    init

--- VariableTest.java
+++ VariableTest.java
@@ -1,5 +1,6 @@
-public class VariableTest {
-   //First Question
+public class VariableTest{
+}
+   First Question
     static int counter=0;
 
     public void incremnetsCounter(){
@@ -11,17 +12,29 @@ public class VariableTest {
         obj1.incremnetsCounter();
          VariableTest obj2=new VariableTest();
         obj2.incremnetsCounter();
-        System.out.println();
-        System.out.println();
+}
  //Second Question
+        public class VariableTest{
+            public static void main (String[]args){
+                int outerX=5;
+                System.out.println("Before block: outerX= "+outerX);
+                if (outerX>0) {
+                    int innerY=15;
+                    System.out.println("İnside block: outerX= "+outerX);
+                    System.out.println("İnside block innerY= "+innerY);
+                }
+              System.out.println("After block: outerX= "+outerX);
+
+            }
+        }
 
 
 
 
 
-    }
 
 
 
-    }
+
+
 
